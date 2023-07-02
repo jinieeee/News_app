4411dd0fbd6cb2e479e264ad7ef584be48821d4d
jinieeee <lje911@gmail.com>
2023-07-02 22:18:15 +0900

    [feat] 리액트 비동기 조회를 활용한 News app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import NewsList from 'components/NewsList';
+// import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   const [data, setData] = useState(null); // data 초기값 null
+//   const onClick = async () => {
+//     const apiKey=process.env.REACT_APP_NEWS_API;
+//     try {
+//       const response = await 
+//         axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=' + apiKey).then(response => {
+//         setData(response.data);
+//       }); 
+      
+//       // 1. axios 비동기 후 callback 함수 실행
+//       // 2. async / await 적용
+//       // 3. news-api 사용 : https://newsapi.org/
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+const App = () => {
+  return <NewsList />;
 }
 
 export default App;
